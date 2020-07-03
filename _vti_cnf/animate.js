938a4b16a125d962ce910cfff0cbeafdf27e666e
kmomocha <66945432+kmomocha@users.noreply.github.com>
2020-07-03 17:31:24 +0530

    alginment corrected
    Alginment corrected and effect removed

--- _vti_cnf/animate.js
+++ _vti_cnf/animate.js
@@ -3,5 +3,5 @@ vti_author:SR|Momocha Kshetrimayum
 vti_modifiedby:SR|Momocha Kshetrimayum
 vti_timecreated:TR|29 Jun 2020 16:02:52 -0000
 vti_timelastmodified:TR|29 Jun 2020 16:02:52 -0000
-vti_backlinkinfo:VX|c-aching\\ legs.htm liver\\ anatomy\\ &\\ function.htm anatomy.htm index.htm c-achilles\\ pain.htm lung\\ anatomy.htm
+vti_backlinkinfo:VX|c-aching\\ legs.htm anatomy.htm index.htm c-achilles\\ pain.htm
 vti_extenderversion:SR|4.0.2.2717
